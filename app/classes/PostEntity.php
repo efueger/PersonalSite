2862a6529707eebe44c955596fa05520e03223d2
Mark Railton <mark@markrailton.com>
2016-12-30 21:13:36 +0000

    Update so that only published posts are displayed

--- app/classes/PostEntity.php
+++ app/classes/PostEntity.php
@@ -7,6 +7,7 @@ class PostEntity
     protected $slug;
     protected $content;
     protected $published;
+    protected $published_at;
 
     /**
      * Accept an array of data matching properties of this class
@@ -14,31 +15,52 @@ class PostEntity
      *
      * @param array $data The data to use to create
      */
-    public function __construct(array $data) {
+    public function __construct(array $data)
+    {
         // no id if we're creating
-        if(isset($data['id'])) {
+        if (isset($data['id'])) {
             $this->id = $data['id'];
         }
 
         $this->title = $data['title'];
         $this->slug = $data['slug'];
         $this->content = $data['content'];
-        $this->published = $data['published'];
+        $this->published_at = $data['published_at'];
+
+        if ($this->published_at == null || $this->published_at > date("Y-m-d H:i:s", time())) {
+            $this->published = false;
+        } else {
+            $this->published = true;
+        }
     }
 
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getTitle() {
+    public function getTitle()
+    {
         return $this->title;
     }
 
-    public function getContent() {
+    public function getContent()
+    {
         return $this->content;
     }
 
-    public function getSlug() {
+    public function getSlug()
+    {
         return $this->slug;
+    }
+
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    public function getPublishedAt()
+    {
+        return $this->published_at;
     }
 }
