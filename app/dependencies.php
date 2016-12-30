c1a8792dd4b34d6ee437ff4fa705c9a4bd3382b1
Mark Railton <mark@markrailton.com>
2016-12-30 19:51:44 +0000

    Updated PDO settings

--- app/dependencies.php
+++ app/dependencies.php
@@ -22,7 +22,7 @@ $container['logger'] = function ($c) {
 
 $container['db'] = function ($c) {
     $db = $c->get('settings')['db'];
-    $pdo = new PDO("mysql:host={$db['host']};dbname={$db['name']},{$db['user']},{$db['pass']}");
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['name'], $db['user'], $db['pass']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
