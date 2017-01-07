6783d275943e52bc2ee12bcf3af70388ff174e11
Mark Railton <mark@markrailton.com>
2017-01-07 21:40:39 +0000

    Moved blog actions into new BlogController

---
+++ app/Controllers/BlogController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Blog\PostEntity;
+use App\Blog\PostMapper;
+use Cocur\Slugify\Slugify;
+use Slim\Exception\NotFoundException;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class BlogController extends BaseController
+{
+    public function index(Request $request, Response $response)
+    {
+        $mapper = new PostMapper($this->db);
+        $posts = $mapper->getPosts();
+
+        return $this->view->render($response, 'posts/index.twig', compact('posts'));
+    }
+
+    public function show(Request $request, Response $response, array $args)
+    {
+        $slug = (string)$args['slug'];
+        $mapper = new PostMapper($this->db);
+        $post = $mapper->getPostBySlug($slug);
+
+        if (!$post) {
+            throw new NotFoundException($request, $response);
+        }
+
+        return $this->view->render($response, 'posts/show.twig', ['post' => $post]);
+    }
+
+    public function new(Request $request, Response $response)
+    {
+        return $this->view->render($response, 'posts/new.twig');
+    }
+
+    public function store(Request $request, Response $response)
+    {
+        $params = $request->getParams();
+        $slugify = new Slugify();
+        $slug = $slugify->slugify($params['title']);
+        $postData = [
+            'title' => $params['title'],
+            'slug' => $slug,
+            'content' => $params['content'],
+            'published_at' => $params['published_at'],
+        ];
+
+        $post = new PostEntity($postData);
+        $postMapper = new PostMapper($this->db);
+        $postMapper->save($post);
+
+        return $response->withRedirect($this->router->pathFor('blog.index'));
+    }
+}
--- app/Routes.php
+++ app/Routes.php
@@ -2,6 +2,7 @@
 
 use App\Blog\PostEntity;
 use App\Blog\PostMapper;
+use App\Controllers\BlogController;
 use App\Middleware\AuthMiddleware;
 use App\Middleware\GuestMiddleware;
 use App\Portfolio\ProjectEntity;
@@ -19,46 +20,10 @@ $app->get('/', function ($request, $response) {
 /**
  * Blog Routes
  */
-$app->get('/blog', function (Request $request, Response $response) {
-    $mapper = new PostMapper($this->db);
-    $posts = $mapper->getPosts();
-
-    return $this->view->render($response, 'posts/index.twig', ['posts' => $posts]);
-})->setName('blog.index');
-
-$app->post('/blog', function (Request $request, Response $response) {
-    $params = $request->getParams();
-    $slugify = new Slugify();
-    $slug = $slugify->slugify($params['title']);
-    $postData = [
-        'title' => $params['title'],
-        'slug' => $slug,
-        'content' => $params['content'],
-        'published_at' => $params['published_at'],
-    ];
-
-    $post = new PostEntity($postData);
-    $postMapper = new PostMapper($this->db);
-    $postMapper->save($post);
-
-    return $response->withRedirect($this->router->pathFor('blog.index'));
-})->add(new AuthMiddleware($container))->setName('blog.new.post');
-
-$app->get('/blog/new', function (Request $request, Response $response) {
-    return $this->view->render($response, 'posts/new.twig');
-})->add(new AuthMiddleware($container))->setName('blog.new');
-
-$app->get('/blog/{slug}', function (Request $request, Response $response, $args) {
-    $slug = (string)$args['slug'];
-    $mapper = new PostMapper($this->db);
-    $post = $mapper->getPostBySlug($slug);
-
-    if (!$post) {
-        throw new NotFoundException($request, $response);
-    }
-
-    return $this->view->render($response, 'posts/show.twig', ['post' => $post]);
-})->setName('blog.show');
+$app->get('/blog', BlogController::class . ':index')->setName('blog.index');
+$app->get('/blog/new', BlogController::class . ':new')->add(new AuthMiddleware($container))->setName('blog.new');
+$app->get('/blog/{slug}', BlogController::class . ':show')->setName('blog.show');
+$app->post('/blog', BlogController::class . ':store')->add(new AuthMiddleware($container))->setName('blog.new.post');
 
 /**
  * Portfolio Routes
