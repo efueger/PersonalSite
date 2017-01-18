5964165e0971f37b25ec15c8afc3722c2e0782d0
Mark Railton <mark@markrailton.com>
2017-01-18 00:21:38 +0000

    Implemented session middleware to remove need to use superglobal

--- app/Middleware/GuestMiddleware.php
+++ app/Middleware/GuestMiddleware.php
@@ -2,10 +2,13 @@
 
 namespace App\Middleware;
 
+use RKA\Session;
+
 class GuestMiddleware extends BaseMiddleware
 {
     public function __invoke($request, $response, $next)
     {
+        $session = new Session();
         if (isset($session->user)) {
             return $response->withRedirect($this->router->pathFor('home'));
         }
